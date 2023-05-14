9077389a031023c91062da035e6ed163dcbff422
Anastasia Vlasova <smm.gorlova@gmail.com>
2023-05-14 12:01:55 +0300

    feat: Добавить инструмент валидации по работе с классами

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+
+
 // Переменные для работы с карточками в галерее фотографий
 const cardTemplate = document.querySelector('#card-template');
 const photoGallerySection = document.querySelector('.photo-gallery');
